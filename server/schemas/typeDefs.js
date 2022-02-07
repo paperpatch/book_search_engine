481409262b7e0f86e6b50cbde3507e0df264fea3
paperpatch <patchen21@gmail.com>
2022-02-07 15:34:46 -0500

    apollo server

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,41 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    savedBooks: [Book]
+  }
+
+  type Book {
+    _id: ID
+    authors: String
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    books(username: String): [Book]
+    Book(_id: ID!): Book
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addBook(thoughtText: String!): Book
+  }
+`;
+
+module.exports = typeDefs;
